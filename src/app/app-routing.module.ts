6b50b11ac42ceb4305364b4d79d109f290ec926e
GustavoSouza07 <gustavosouzaloure@gmail.com>
2024-02-21 14:01:39 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { CardInformationComponent } from './components-content/card-information/card-information.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 const roteamento: Routes = [
-    {
-      path:'',
-      component:HomeComponent,
-    },
-
-    {
-      path:'developerPage',
-      component:ContentComponent,
-    }
-
+    {path:'',component:HomeComponent,},
+    {path:'developerPage', component:ContentComponent,},
 ];
 
 @NgModule({
